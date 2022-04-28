88093a82400413d1cc46c0390138e2a2be7ea54c
Osman Pérez <osman2907@gmail.com>
2022-04-27 20:47:31 -0400

    Criterios de busqueda y ruta para registrar

--- consultas.js
+++ consultas.js
@@ -8,15 +8,57 @@ const pool = new Pool({
     port: 5432
 });
 
-const listar = async () => {
+const listar = async (criterios) => {
     const config = {
         text:"SELECT * FROM prendas",
         values: []
     }
 
+    const llaves = Object.keys(criterios);
+    llaves.forEach((item, index) => {
+        // Ejemplo 1
+        // if(index == 0){
+        //     config.text += ` WHERE ${item} ilike '%${criterios[item]}%'`
+        // } else {
+        //     config.text += ` AND ${item} ilike '%${criterios[item]}%'`
+        // }
+
+        // Ejemplo 2 (Recomendado)
+        if(index == 0){
+            config.text += ` WHERE ${item} ilike $${index+1}`
+        } else {
+            config.text += ` AND ${item} ilike $${index+1}`
+        }
+        config.values.push(`%${criterios[item]}%`)
+    });
+    // console.log(criterios);
+    // console.log(config);
+
     const resp = await pool.query(config);
     return resp;
 }
 
-module.exports = { listar }
+
+const registrar = async (prenda) => {
+    const { nombre, color } = prenda;
+    const config = {
+        text: "INSERT INTO prendas(nombre, color) VALUES($1, $2) RETURNING *",
+        values: [nombre, color]
+    }
+
+    try {
+        const resp = await pool.query(config);
+        return {
+            message: 'Registro realizado exitosamente',
+            prenda: resp.rows[0]
+        }
+    } catch (error) {
+        return {
+            message: "Ocurrió un error registrando prenda"
+        }
+    }
+    
+}
+
+module.exports = { listar, registrar }
 
--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
-const { listar } = require('./consultas.js');
+const { listar, registrar } = require('./consultas.js');
 
 app.listen(3000, () => console.log("Servidor activo http://localhost:3000"))
 
+app.use(express.json());
+
 app.get("/prendas", async (req, res) => {
-    const listado = await listar();
+    const listado = await listar(req.query);
     res.json({ prendas: listado.rows });
+})
+
+
+app.post("/prendas", async(req, res) => {
+    const respuesta = await registrar(req.body);
+    res.status(respuesta.prenda ? 201 : 500).json(respuesta);
 })
